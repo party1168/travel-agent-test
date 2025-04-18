5a66db04e53effe6b9f6ed51273d1e53e290c2ef
party1168 <a2775269@gmail.com>
2025-04-18 22:26:48 +0800

    feature: 新增 logger.middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { FlightModule } from './modules/flight/flight.module';
+import { LoggerMiddleware } from './middleware/logger/logger.middleware';
 
 @Module({
   imports: [
@@ -15,4 +16,8 @@ import { FlightModule } from './modules/flight/flight.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/middleware/logger/logger.middleware.ts
+++ src/middleware/logger/logger.middleware.ts
@@ -1,8 +1,11 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
+  private readonly logger = new Logger(LoggerMiddleware.name);
+  use(req: Request, res: Response, next: () => void) {
+    this.logger.log(`Request... ${req.method} ${req.originalUrl} `);
     next();
   }
 }
